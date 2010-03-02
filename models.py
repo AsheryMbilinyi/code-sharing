69fdf6568fa6784ba0c26c6ba107e719b20d41ed
Graham Greenfield <grahamg@gmail.com>
2010-03-01 23:59:19 -0600

    added Languages model

--- models.py
+++ models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from pygments import lexers
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    language_code = models.CharField(max_length=50)
+    mime_type = models.CharField(max_length=100)
+    
+    class Meta:
+        ordering = ['name']
+
+    def get_lexer(self):
+        return lexers.get_lexer_by_name(self.language_code)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return ('cab_language_detail', (), {'slug': self.slug})
+    
+    get_absolute_url = models.permalink(get_absolute_url)
